091d0ae2aa3570bd5eaefad482eed0f4a75c3a1e
Masterr.UA <Masterr.UA@3efe3d2e-8cf4-11de-88c3-552db54a6a53>
2009-08-28 21:16:20 +0000

    N-260809-1

--- trunk/billiwall/app/controllers/users_controller.php
+++ trunk/billiwall/app/controllers/users_controller.php
@@ -99,7 +99,7 @@ class UsersController extends AppController {
                         $server->enableUser($user['User']['id']);                        
                         $this->data['User']['blocked']=false;
                     }
-		if ($this->data['User']['balance']<$user['UnlimitedTariff']['value']) $this->data['User']['blocked']=false;
+		if ($this->data['User']['balance']<$user['UnlimitedTariff']['value']) $this->data['User']['blocked']=true;
 
 		//Перезаписываем скорость на сервере
                 $server->changeUserSpeed($id, $unlimited_tariff['UnlimitedTariff']['upload_speed'], $unlimited_tariff['UnlimitedTariff']['download_speed'], $this->data['User']['vpn_ip']);
