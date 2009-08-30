e58275818957f7a58298f2aed457227964e48424
Masterr.UA <Masterr.UA@3efe3d2e-8cf4-11de-88c3-552db54a6a53>
2009-08-30 17:54:16 +0000

    N-260809-1

--- billiwall/app/controllers/nocs.php
+++ billiwall/app/controllers/nocs.php
@@ -2,6 +2,7 @@
 class NocsController extends AppController {
     var $name='Nocs';
     var $uses=null;
+    var $helpers = array('Html', 'Javascript', 'Ajax'); 
 
     function index() {
 	$this->set('nocs', $this->User->find('all'));
